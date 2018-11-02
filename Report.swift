132bedae92d22d3b4aaec6129e887a51e24931e6
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2018-11-01 23:48:20 -0600

    Added Report Class

---
+++ Report.swift
@@ -0,0 +1,14 @@
+//
+//  Report.swift
+//  WeAreConnected
+//
+//  Created by Rodrigo Carballo on 11/1/18.
+//  Copyright © 2018 Rodrigo Carballo. All rights reserved.
+//
+
+import Foundation
+
+struct Report {
+    var title : String = ""
+    var href : URL
+}
