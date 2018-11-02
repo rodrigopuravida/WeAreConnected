ee6ee926d48f873f9f0ab633460b00ad7c44ddd0
Rodrigo Carballo <rodrigo.puravida@gmail.com>
2018-11-01 22:41:00 -0600

    Initial Commit

---
+++ WeAreConnected/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WeAreConnected
+//
+//  Created by Rodrigo Carballo on 11/1/18.
+//  Copyright © 2018 Rodrigo Carballo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WeAreConnected/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WeAreConnected
+//
+//  Created by Rodrigo Carballo on 11/1/18.
+//  Copyright © 2018 Rodrigo Carballo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
